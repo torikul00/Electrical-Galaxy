0df8cc226ba07ade03d882e6dd464ddf1d598a74
Torikul <toriqulislam142@gmail.com>
2022-04-16 12:06:19 -0500

    created all pages folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Home from './components/Home/Home';
-import Navbar from './components/Home/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp.css'
+
+const SignUp = () => {
+    return (
+        <div>
+        <h1>this is signup page</h1>
+        </div>
+    );
+};
+
+export default SignUp;
