7ebe25b2a61d2e15afb5b37878bc8e98a8a148af
Torikul <toriqulislam142@gmail.com>
2022-04-17 03:24:54 -0500

    install react icon

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is login page</h1>
-        </div>
+        <div className='form-container'>
+        <form >
+            <h1 className='form-title'>Please Login</h1>
+            <div className="inputs">
+                <input type="email" placeholder='Email' name='email' />
+
+                <input  type="password" placeholder='Password' name='password' /> <br />
+                <p  className='forgot-password'>Forgot Password ?</p>
+                <button type='submit' className='button'>Login</button>
+                <p className='signup-link'>New User ? <Link to="/signup">Sign Up </Link></p>
+
+            </div>
+
+            <div className="horizontal">
+                <div className='line' />
+                <p>OR</p>
+                <div className='line' />
+
+            </div>
+            <div className="icons">
+                {/* <FcGoogle  className='google-icon' />
+                <BsFacebook  className='facebook-icon' />
+                <BsGithub  className='github-icon' /> */}
+            </div>
+
+
+        </form>
+
+    </div>
     );
 };
 
